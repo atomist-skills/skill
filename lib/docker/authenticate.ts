854c16151a2549f0290907b0d21a9c5e98169c9d
Christian Dupuis <cd@atomist.com>
2022-07-12 08:22:57 +0200

    Add registry log

--- lib/docker/authenticate.ts
+++ lib/docker/authenticate.ts
@@ -22,8 +22,9 @@ import {
 	DockerRegistryType,
 } from "../definition/subscription/common_types";
 import { Contextual, EventContext } from "../handler/handler";
-import { warn } from "../log/index";
+import { debug, warn } from "../log/index";
 import { createFile } from "../tmp_fs";
+import { replacer } from "../util";
 
 export async function doAuthed<T>(
 	ctx: EventContext<any, any>,
@@ -59,6 +60,13 @@ export async function authenticate(
 	if (process.env.ATOMIST_SKIP_DOCKER_AUTH) {
 		return;
 	}
+	debug(
+		`Authenticating using registries: ${JSON.stringify(
+			registries,
+			replacer,
+			undefined,
+		)}`,
+	);
 	const dockerConfig = {
 		auths: {},
 	} as any;
