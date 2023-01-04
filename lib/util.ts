dfd073f689a37c661fa0fb77352b9abf3c500a86
Christian Dupuis <cd@atomist.com>
2023-01-04 19:24:11 +0100

    Change staging detection

--- lib/util.ts
+++ lib/util.ts
@@ -260,7 +260,7 @@ export function isStaging(): boolean {
 	return (
 		process.env.ATOMIST_GRAPHQL_ENDPOINT ||
 		"https://automation.atomist.com/graphql"
-	).includes(".services");
+	).includes(".stage");
 }
 
 export function pluralize(
